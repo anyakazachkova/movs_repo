0cb542cbbf4223f724ff6f187d67c1c029a456e5
anyakazachkova <anya.kazachkova98@gmail.com>
2022-11-02 12:18:18 +0600

    v6

--- task-fastapi/main.py
+++ task-fastapi/main.py
@@ -7,41 +7,41 @@ from typing import Union
 from typing import List
 from collections import OrderedDict
 from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
-import json
 
 app = FastAPI()
 
 items = [] #пустой список, который будем дальше пополнять
 
+#Реализация пути "/"
 @app.get('/')
 async def root():
     return {'message': 'Hello'}
 
+#Реализация пути "/post"
 @app.post('/post')
 async def get_post():
     return {'id': '0', 'timestamp': '0'}
 
-
+#Класс для пород собак
 class DogType(str, Enum):
     dog1 = 'terrier'
     dog2 = 'bulldog'
     dog3 = 'dalmatian'
 
-
+#Класс для собаки
 class Dog(BaseModel):
     name: str
     pk: Union[int, None] = None
     kind: DogType
 
+#Реализация записи собак
 @app.post('/dog')
 async def create_dog(dog: Dog):
     dog.pk = len(items)
     items.append(dog)
     return dog
 
-
-
+#Реализация загрузки списка собак, в том числе отдельно по породам
 @app.get('/dog')
 async def get_dogs(kind: DogType = None):
     if kind == None:
@@ -53,7 +53,7 @@ async def get_dogs(kind: DogType = None):
                 answer.append(i)
         return answer
 
-
+#Реализация получения информации о собаке по id
 @app.get('/dog/{pk}')
 async def get_dog_by_pk(pk: int):
     if (pk < 0) or (pk > len(items)-1):
@@ -63,12 +63,12 @@ async def get_dog_by_pk(pk: int):
         return dog_info
 
 
-
-@app.patch('/dog/{pk}', response_model=Dog)
+#Реализация обновления собаки по id
+@app.patch('/dog/{pk}')
 async def update_dog(pk: int, dog: Dog):
-    stored_model = Dog(name=(items[pk]).name, pk=(items[pk]).pk, kind=(items[pk]).kind)
-    updated_data = dog.dict(exclude_unset=True)
-    updated_item = stored_model.copy(update=updated_data)
-    items[pk] = jsonable_encoder(stored_model)
-    return items
-
+    stored_dog_data = dict(items[pk])
+    stored_dog_model = Dog(name = stored_dog_data['name'], pk = pk, kind = stored_dog_data['kind'])
+    new_data = dog.dict(exclude_unset=True)
+    updated_dog = stored_dog_model.copy(update=new_data)
+    items[pk] = jsonable_encoder(updated_dog)
+    return updated_dog
